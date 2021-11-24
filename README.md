c9b72cec3b08db57f4113ecfd7014ce4e9da3da2
Carlo Teubner <carlo@cteubner.net>
2021-11-24 13:29:32 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+# gconsentfox
+
+Toool that inserts the cookies needed to get rid of Google's annoying cookie
+consent popups into Firefox's cookie database, and then starts Firefox.
+
+For use with "Delete cookies and site data when Firefox is closed" enabled
+in _Settings > Privacy & Security_.
---
+++ gconsentfox.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import configparser
+import datetime
+import os
+import pathlib
+import sqlite3
+import sys
+
+
+COOKIES = (  # name, value, host
+    ("ANID", "OPT_OUT", ".google.com"),
+    ("ANID", "OPT_OUT", ".google.co.uk"),
+    ("CONSENT", "YES+cb", ".google.com"),
+    ("CONSENT", "YES+cb", ".google.co.uk"),
+    ("CONSENT", "YES+cb", ".youtube.com"),
+)
+
+SQL = """\
+INSERT OR IGNORE INTO moz_cookies
+    (originAttributes, name, value, host, path, expiry, lastAccessed, creationTime,
+     isSecure, isHttpOnly, inBrowserElement, sameSite, rawSameSite, schemeMap)
+  VALUES ('', ?, ?, ?, '/', ?, ?, ?, 1, 1, 0, 0, 0, 2)\
+"""
+
+
+def get_cookies_filename():
+    firefox_path = pathlib.Path.home() / ".mozilla/firefox"
+    profiles = configparser.ConfigParser()
+    profiles.read(firefox_path / "profiles.ini")
+    for section in profiles.sections():
+        if section.startswith("Install"):
+            profile = profiles[section]["Default"]
+            return firefox_path / profile / "cookies.sqlite"
+
+
+def main():
+    print("Inserting Google cookie consent cookies...")
+    db = sqlite3.connect(get_cookies_filename(), timeout=0.25)
+    cur = db.cursor()
+    expiry_ts = datetime.datetime(2100, 1, 1, tzinfo=datetime.timezone.utc).timestamp()
+    now_tsms = datetime.datetime.now(tz=datetime.timezone.utc).timestamp() * 1000000
+    try:
+        for cookie in COOKIES:
+            cur.execute(SQL, (*cookie, expiry_ts, now_tsms, now_tsms))
+    except sqlite3.OperationalError as e:
+        print(f"Failed to add cookies: {e}. Firefox already running?")
+    db.commit()
+    db.close()
+    print("Starting Firefox...")
+    os.execv("/usr/bin/firefox", sys.argv)
+
+
+main()
